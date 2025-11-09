f15ca768e77a2c846c6154e33bffcdd03c9209d9
4yulming4 <yulming172@gmail.com>
2025-11-09 17:24:36 -0500

    preferences.py has funct. that may be useful later

--- backend/data/preferences.py
+++ backend/data/preferences.py
@@ -1,4 +1,58 @@
 from enum import Enum
+import pandas as pd
+
+class drive_train_enum(Enum):
+    FWD = "FWD"
+    twoWD = "2WD"
+    AWD = "AWD"
+    fourWD = "4WD"
+    RWD = "RWD"
+    ADW = "ADW"
+    fourx2 = "4x2"
+    fourx4 = "4x4"
+
+class vehicle_type_enum(Enum):
+    PC = "PC"
+    MPV = "MPV"
+    PHEV = "PHEV"
+    BEV = "BEV"
+    SUV = "SUV"
+    four_DR = "4 DR"
+    TRUCK = "TRUCK"
+    PU_CC = "PU/CC"
+    five_HB = "5 HB"
+    BUS = "BUS" #all buses will probably be dropped from database but just in case
+    MPV_BUS = "MPV_BUS" #see above
+    VAN = "VAN"
+    SPORT_UTILITY_VEHICLE = "SPORT UTILITY VEHICLE" #originally space instead of _
+    HEAVY_PASSENGER_CAR = "HEAVY PASSENGER CAR"
+    MEDIUM_PASSENGER_CAR = "MEDIUM PASSENGER CAR"
+    LIGHT_PASSENGER_CAR = "LIGHT PASSENGER CAR"
+    COMPACT_PASSENGER_CAR = "COMPACT PASSENGER CAR"
+    PICKUP = "PICKUP"
+    MINI_PASSENGER_CAR = "MINI PASSENGER CAR"
+
+class overall_stars(Enum):
+    one = "1"
+    two = "2"
+    three = "3"
+    four = "4"
+    five = "5"
+
+#excludes a lot of options that are like 5 or 7
+#will include more once I confirm this works
+class num_of_seating(Enum):
+    one = ["1", "1 to 2", "1 to 5"]
+    two = ["2"]
+    three = ["3"]
+    four = ["4"]
+    five = ["5", "57,", '"5, 7"', ]
+    six = ["6"]
+    seven = ["7"]
+    eight = ["8"]
+    nine = ["9"]
+    twelve = ["12"]
+    fifteen = ["15"]
 
 #'MAKE'
 #'MODEL'
@@ -8,34 +62,26 @@ from enum import Enum
 #'NUM_OF_SEATING' int
 #'OVERALL_STARS' int between 0 and 5 inclusive
 
-class drive_train(Enum):
-    FWD = 1
-    twoWD = 2 #originally 2WD, same applies to other uses of "two" and "four"
-    AWD = 3
-    fourWD = 4
-    RWD = 5
-    ADW = 6
-    fourx2 = 7
-    fourx4 = 8
+#flags are defined as enums
+#sort flags by type?
+def filterCars(database, flags_list = []):
+    for x in flags_list:
+        if isinstance(x, drive_train_enum):
+            database = database.loc[database['DRIVE_TRAIN'] == x._value_]
+        elif isinstance(x, vehicle_type_enum):
+            database = database.loc[database['VEHICLE_TYPE'] == x._value_]
+        elif isinstance(x, num_of_seating):
+            database = database[database['NUM_OF_SEATING'].isin(x._value_)]
+        elif isinstance(x, overall_stars):
+            database = database.loc[database['OVERALL_STARS'] == x._value_]
+    return database
 
-class vehicle_type(Enum):
-    PC = 1
-    MPV = 2
-    PHEV = 3
-    BEV = 4
-    SUV = 5
-    four_DR = 6 #originally "4 DR"
-    TRUCK = 7
-    PU_CC = 8 #originally PU/CC
-    five_HB = 9 #originally "5 HB"
-    BUS = 10 #all buses will be dropped from database
-    MPV/BUS = 11 #see above
-    VAN = 12
-    SPORT_UTILITY_VEHICLE = 13 #originally space instead of _
-    HEAVY_PASSENGER_CAR = 14 #above applies this all other spaces
-    MEDIUM_PASSENGER_CAR = 15
-    LIGHT_PASSENGER_CAR = 16
-    COMPACT_PASSENGER_CAR = 17
-    PICKUP = 18
-    MINI_PASSENGER_CAR = 19
+#creates a ranking column
+def wants(database, flags_list = []):
+    return database
 
+df = pd.read_csv("backend\\data\\nhtsa_prepared.csv", dtype = str)
+test_flags = [overall_stars.five, num_of_seating.four]
+new_df = filterCars(df,test_flags)
+print(new_df)
+#print(df.loc[df['NUM_OF_SEATING'] == "4"])
